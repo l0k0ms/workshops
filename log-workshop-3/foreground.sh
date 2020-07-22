18c132b0f92fa97c1ddfb14281c9d5ad9e33759e
l0k0ms <pierre.guceski@datadoghq.com>
2020-07-22 10:17:28 +0200

    fix

--- log-workshop-3/foreground.sh
+++ log-workshop-3/foreground.sh
@@ -2,24 +2,20 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-
-# clear
-
 # while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented-no-log.yml ]; do sleep 2; done
 
-echo "Downloading ressources for workshop"
 until [ -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented-no-log.yml ]
 do
-    echo "."
     sleep 2
 done
-echo "Workshop resources downloaded"
+clear
 
 cd /root/app-files/docker-compose-files
 
-# clear
+clear
 
 docker-compose -f docker-compose-fixed-instrumented-no-log.yml up -d
-# clear
+
+clear
 
 prep-environment
