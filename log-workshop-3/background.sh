5f32a0079d18daa42b229a4043683abdcff35090
l0k0ms <pierre.guceski@datadoghq.com>
2020-07-22 10:08:03 +0200

    info boot

--- log-workshop-3/background.sh
+++ log-workshop-3/background.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 mkdir /root/app-files
+
+echo "Workshop folder created !" | wall -n
+
+echo "Cloning Application repository !" | wall -n
+
 git clone https://github.com/l0k0ms/ecommerce-workshop /root/app-files
+
+echo "Cloning Application repository: Successful " | wall -n
+
 cd /root/app-files
+
 git checkout gus/log-workshop
+
+echo "Pulling containers" | wall -n
+
 docker-compose pull
 
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- log-workshop-3/foreground.sh
+++ log-workshop-3/foreground.sh
@@ -2,10 +2,22 @@
 
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
+
 clear
-while [ ! -f /root/app-files/docker-compose-files/docker-compose-broken-no-apm-instrumentation.yml ]; do sleep 2; done
+
+printf '\nPreparing the environment\n'
+
+until $(! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented-no-log.yml); do
+    printf '.'
+    sleep 2
+done
+
 cd /root/app-files/docker-compose-files
+
 clear
+
 docker-compose -f docker-compose-fixed-instrumented-no-log.yml up -d
+
 clear
+
 prep-environment
