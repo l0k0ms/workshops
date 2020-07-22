991b4a83ab3b8609b806fc48d1e46170484874c6
l0k0ms <pierre.guceski@datadoghq.com>
2020-07-22 09:17:29 +0200

    updating scripts

--- log-workshop-3/background.sh
+++ log-workshop-3/background.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir /root/app-files
-git clone https://github.com/DataDog/ecommerce-workshop /root/app-files
+git clone https://github.com/l0k0ms/ecommerce-workshop /root/app-files
 cd /root/app-files
+git checkout gus/log-workshop
 docker-compose pull
 
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- log-workshop-3/foreground.sh
+++ log-workshop-3/foreground.sh
@@ -6,6 +6,6 @@ clear
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-broken-no-apm-instrumentation.yml ]; do sleep 2; done
 cd /root/app-files/docker-compose-files
 clear
-docker-compose -f docker-compose-broken-no-apm-instrumentation.yml up -d
+docker-compose -f docker-compose-fixed-instrumented-no-log.yml up -d
 clear
 prep-environment
