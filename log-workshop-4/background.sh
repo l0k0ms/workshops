6472f420438f4f557ec824ac665c37c3df6da091
l0k0ms <pierre.guceski@datadoghq.com>
2020-07-24 10:30:41 +0200

    fixes

--- log-workshop-4/background.sh
+++ log-workshop-4/background.sh
@@ -4,9 +4,9 @@ sleep 1
 
 touch ~/.bashrc
 
-echo "alias app_start='docker-compose -f docker-compose-fixed-instrumented-with-log.yml up -d'">> ~/.bashrc
-echo "alias app_start_with_logs='docker-compose -f docker-compose-fixed-instrumented-with-log.yml up'">> ~/.bashrc
-echo "alias app_stop='docker-compose -f docker-compose-fixed-instrumented-with-log.yml stop && docker-compose -f docker-compose-fixed-instrumented-with-log.yml rm -f'">> ~/.bashrc
+echo "alias app_start='docker-compose -f docker-compose-fixed-instrumented.yml up -d'">> ~/.bashrc
+echo "alias app_start_with_logs='docker-compose -f docker-compose-fixed-instrumented.yml up'">> ~/.bashrc
+echo "alias app_stop='docker-compose -f docker-compose-fixed-instrumented.yml stop && docker-compose -f docker-compose-fixed-instrumented.yml rm -f'">> ~/.bashrc
 echo "alias app_restart='app_stop && app_start'">> ~/.bashrc
 
 echo "alias generating_logs='docker run -d  -it --rm mingrammer/flog -f apache_combined -l -n 100000 -d 0.2'">>~/.bashrc
@@ -25,5 +25,3 @@ echo "Cloning Application repository: Successful " | wall -n
 echo "Pulling containers" | wall -n
 
 docker-compose pull
-
-source ~/.bashrc
--- log-workshop-4/foreground.sh
+++ log-workshop-4/foreground.sh
@@ -6,5 +6,6 @@ export POSTGRES_PASSWORD=postgres
 while [ ! -f /root/app-files/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
 
 cd /root/app-files/docker-compose-files
+source ~/.bashrc
 
 clear
