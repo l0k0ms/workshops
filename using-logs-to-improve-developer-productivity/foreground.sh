b461cfcaeee042fbdcdb9bd10f3e83254732574c
l0k0ms <pierre.guceski@datadoghq.com>
2021-10-06 09:24:11 +0200

    fix

--- using-logs-to-improve-developer-productivity/foreground.sh
+++ using-logs-to-improve-developer-productivity/foreground.sh
@@ -3,7 +3,7 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 
-while [ ! -f /root/app-files/deploy/docker-compose-files/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
+while [ ! -f /root/app-files/deploy/docker-compose/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
 
 echo "Cloning Application repository: Successful"
 
