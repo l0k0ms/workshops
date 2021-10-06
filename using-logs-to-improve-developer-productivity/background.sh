fe029d36681c7eb6398e8f89dc2a08fd28f02a75
l0k0ms <pierre.guceski@datadoghq.com>
2021-10-06 09:33:38 +0200

    fixing script

--- using-logs-to-improve-developer-productivity/background.sh
+++ using-logs-to-improve-developer-productivity/background.sh
@@ -4,18 +4,4 @@ curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 sleep 1
 
-echo "Initialization, please wait" | wall -n
-
-touch ~/.bashrc
-
-echo "alias app_start='docker-compose -f docker-compose-fixed-instrumented.yml up -d'">> ~/.bashrc
-echo "alias app_start_with_logs='docker-compose -f docker-compose-fixed-instrumented.yml up'">> ~/.bashrc
-echo "alias app_stop='docker-compose -f docker-compose-fixed-instrumented.yml stop && docker-compose -f docker-compose-fixed-instrumented.yml rm -f'">> ~/.bashrc
-echo "alias app_restart='app_stop && app_start'">> ~/.bashrc
-
-echo "alias generating_traffic='cd ../ && ./gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\" >> /dev/null 2>&1 && cd ./docker-compose-files &'" >> ~/.bashrc
-echo "alias creating_issue='docker kill docker-compose-files_db_1 >> /dev/null 2>&1 &'" >> ~/.bashrc
-
-echo "alias generating_logs='docker run -d  -it --rm mingrammer/flog -f apache_combined -l -n 100000 -d 0.2'">>~/.bashrc
-
 statusupdate complete
--- using-logs-to-improve-developer-productivity/foreground.sh
+++ using-logs-to-improve-developer-productivity/foreground.sh
@@ -3,9 +3,19 @@
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 
-while [ ! -f ../ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml ]; do sleep 2; done
-
 cd ../ecommworkshop/deploy/docker-compose/
+
+touch ~/.bashrc
+
+echo "alias app_start='docker-compose -f docker-compose-fixed-instrumented.yml up -d'">> ~/.bashrc
+echo "alias app_start_with_logs='docker-compose -f docker-compose-fixed-instrumented.yml up'">> ~/.bashrc
+echo "alias app_stop='docker-compose -f docker-compose-fixed-instrumented.yml stop && docker-compose -f docker-compose-fixed-instrumented.yml rm -f'">> ~/.bashrc
+echo "alias app_restart='app_stop && app_start'">> ~/.bashrc
+
+echo "alias generating_traffic='cd ../ && ./gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\" >> /dev/null 2>&1 && cd ./docker-compose-files &'" >> ~/.bashrc
+echo "alias creating_issue='docker kill docker-compose-files_db_1 >> /dev/null 2>&1 &'" >> ~/.bashrc
+
+echo "alias generating_logs='docker run -d  -it --rm mingrammer/flog -f apache_combined -l -n 100000 -d 0.2'">>~/.bashrc
 
 source ~/.bashrc
 
