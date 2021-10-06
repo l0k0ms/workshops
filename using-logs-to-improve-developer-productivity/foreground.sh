ce1c7761da28fa6b746e8cfc8c3ccd27ffc88bf7
l0k0ms <pierre.guceski@datadoghq.com>
2021-10-06 18:09:55 +0200

    updating images

--- using-logs-to-improve-developer-productivity/foreground.sh
+++ using-logs-to-improve-developer-productivity/foreground.sh
@@ -12,8 +12,8 @@ echo "alias app_start_with_logs='docker-compose -f docker-compose-fixed-instrume
 echo "alias app_stop='docker-compose -f docker-compose-fixed-instrumented.yml stop && docker-compose -f docker-compose-fixed-instrumented.yml rm -f'">> ~/.bashrc
 echo "alias app_restart='app_stop && app_start'">> ~/.bashrc
 
-echo "alias generating_traffic='cd ../ && ./gor --input-file-loop --input-file requests_0.gor --output-http \"http://localhost:3000\" >> /dev/null 2>&1 && cd ./docker-compose-files &'" >> ~/.bashrc
-echo "alias creating_issue='docker kill docker-compose-files_db_1 >> /dev/null 2>&1 &'" >> ~/.bashrc
+echo "alias generating_traffic='docker-compose -f docker-compose-traffic-replay.yml up -d'" >> ~/.bashrc
+echo "alias creating_issue='docker kill docker-compose_db_1 >> /dev/null 2>&1 &'" >> ~/.bashrc
 
 echo "alias generating_logs='docker run -d  -it --rm mingrammer/flog -f apache_combined -l -n 100000 -d 0.2'">>~/.bashrc
 
